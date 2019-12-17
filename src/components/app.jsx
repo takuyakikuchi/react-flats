776eff2dc03e90344e5f41a3127e864ae5f6347a
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-12-17 15:32:21 +0900

    added flat_list.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 // import GoogleMapReact from 'google-map-react';
 
+import flats from '../../data/flats';
+import FlatList from './flat_list';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected_flat: "",
-      // flats
+      selectedFlat: flats[0],
+      flats
     };
   }
 
@@ -17,10 +20,19 @@ class App extends Component {
   //   };
   // }
 
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
   render() {
     return (
       <div>
         <div className="flat-list">
+          <FlatList
+            flats={this.state.flats}
+            selectedFlat={this.state.selectedFlat}
+            selectFlat={this.selectFlat}
+          />
         </div>
         <div className="map-container">
         </div>
